e9d5293a4d7224dc16fb40b6c2344a819c7d8de7
antoncoding <b03705027@ntu.edu.tw>
2019-03-02 13:42:31 +0800

    static settings and .pyc removal

--- grusse/settings.py
+++ grusse/settings.py
@@ -125,7 +125,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# 無論檔案放在哪裡，都可以使用/static/訪問到
 STATIC_URL = '/static/'
+
+# 开发阶段放置项目自己的静态文件
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+
+# 执行collectstatic命令后会将项目中的静态文件收集到该目录下面来（所以不应该在该目录下面放置自己的一些静态文件，因为会覆盖掉）
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Add Custom User Template
 AUTH_USER_MODEL = 'users.CustomUser' 
--- inbox/views.py
+++ inbox/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import loader
 
 # for testing
 from django.http import HttpResponse
@@ -6,3 +7,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("inbox.index .")
+
+# render Template page
+def template(request):  
+    context = {'name': 'qq'}
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(context, request))
